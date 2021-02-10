ad1c6080e26dcb9796cd195a967b7bb1929c67dd
Rafael Rufino <rafael.rufino@muchmore.digital>
2021-02-10 01:09:48 -0300

    Adicionar arquivos de projeto.

---
+++ Extended_System_Library/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Extended_System_Library")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Extended_System_Library")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("227e2f38-c75c-4978-b170-fdc37df11388")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' utilizando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
